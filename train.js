d892822a0c6b9b508c82de497cd48d23eb4d2fe3
NATHAN4627 <okilovnodir456@gmail.com>
2026-08-30 05:14:57 +0900

    MIT task commited

--- train.js
+++ train.js
@@ -1,35 +1,52 @@
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
-
-// CALLBACK FUNCTIONS
-
-function giveAdvice(a, callback) {
-  if (typeof a !== "number") callback("Enter numbers only", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function(){
-      callback(null, list[5]);
-    },1000)
+//MIT TASK
+function countLetter(a, b) {
+  let count = 0;
+  for (let i = 0; i < b.length; i++) {
+    if (b[i] === a) {
+      count++;
+    }
   }
+
+  if (count === 0) {
+    console.log("There is no any letter here");
+  }
+  return count;
 }
 
-giveAdvice(31, (err, data) => {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log("Error", err);
-  }
-});
+console.log(countLetter("e", "engineer"));
+
+// const list = [
+//   "yahshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// // CALLBACK FUNCTIONS
+
+// function giveAdvice(a, callback) {
+//   if (typeof a !== "number") callback("Enter numbers only", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function(){
+//       callback(null, list[5]);
+//     },1000)
+//   }
+// }
+
+// giveAdvice(31, (err, data) => {
+//   if (data) {
+//     console.log(data);
+//   } else {
+//     console.log("Error", err);
+//   }
+// });
 
 //ASYNC FUNCTIONS
 
